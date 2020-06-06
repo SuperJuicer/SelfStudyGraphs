6e61f2940aaa80f3bd960d953a2aebee51c6722c
SuperJuice <wisconsinsara@gmail.com>
2020-06-05 21:20:31 -0700

    Add project files.

---
+++ SelfStudyGraphs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SelfStudyGraphs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Graphs!");
+        }
+    }
+}
