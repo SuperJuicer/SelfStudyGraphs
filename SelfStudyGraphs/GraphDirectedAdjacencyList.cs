43f1f8c539d4d915e4506da7e4641a630a5b33a0
SuperJuice <wisconsinsara@gmail.com>
2020-06-29 11:20:45 -0700

    Added error checking and cases.

--- SelfStudyGraphs/GraphDirectedAdjacencyList.cs
+++ SelfStudyGraphs/GraphDirectedAdjacencyList.cs
@@ -9,9 +9,9 @@ namespace SelfStudyGraphs
     class GraphDirectedAdjacencyList
     {
         /// <summary>
-        /// The number of vertices (nodes) in the graph
+        /// The number of vertices (nodes) in the graph, limited to int.
         /// </summary>
-        private int numVertices;
+        internal int numVertices;
 
         /// <summary>
         /// The adjacency array of lists
@@ -24,6 +24,12 @@ namespace SelfStudyGraphs
         /// <param name="numVertices">The total number of vertices (nodes) in the graph</param>
         internal GraphDirectedAdjacencyList(int numVertices)
         {
+            // Check for at least 1 vertex.
+            if (numVertices < 1)
+            {
+                throw new ArgumentOutOfRangeException("Graph must have at least 1 vertex.");
+            }
+
             this.numVertices = numVertices;
             adj = new List<int>[numVertices];
 
@@ -34,12 +40,24 @@ namespace SelfStudyGraphs
         }
 
         /// <summary>
-        /// Creates an edge between the given vertices
+        /// Creates an edge between the given vertices. Vertices may have edges to themselves.
         /// </summary>
         /// <param name="vertex1">The vertex to which to add a directed edge</param>
         /// <param name="vertex2">The vertex to which an edge is directed</param>
         internal void AddEdge(int vertex1, int vertex2)
         {
+            // Check for out of range vertex1.
+            if (vertex1 < 0 || vertex1 >= numVertices)
+            {
+                throw new ArgumentOutOfRangeException($"The first parameter must be at least 0 and less than {numVertices}.");
+            }
+
+            // Check for out of range vertex2.
+            if (vertex2 < 0 || vertex2 >= numVertices)
+            {
+                throw new ArgumentOutOfRangeException($"The second parameter must be at least 0 and less than {numVertices}.");
+            }
+
             adj[vertex1].Add(vertex2);
         }
 
@@ -49,6 +67,12 @@ namespace SelfStudyGraphs
         /// <param name="startingVertex">Vertex where printing starts</param>
         internal void PrintVerticesBreathFirst(int startingVertex)
         {
+            // Check for out of bounds startingVertex
+            if (startingVertex < 0 || startingVertex >= numVertices)
+            {
+                Console.WriteLine($"Parameter must be at least 0 and less than {numVertices}.");
+                return;
+            }
             // The graph may have a cycle, so store visited vertices.
             bool[] visited = new bool[numVertices];
             visited[startingVertex] = true;
--- SelfStudyGraphs/Program.cs
+++ SelfStudyGraphs/Program.cs
@@ -16,8 +16,27 @@ namespace SelfStudyGraphs
             graph1.AddEdge(2, 3);
             graph1.AddEdge(3, 3);
 
-            Console.WriteLine("\nHere's a breadth first traversal of graph1 starting from vertex 2:");
+            // A graph with no vertices throws an exception:
+            // var graph2 = new GraphDirectedAdjacencyList(0);
+
+            // Trying to add impossible edges throws exceptions:
+            // graph1.AddEdge(-1, 0);
+            // graph1.AddEdge(graph1.numVertices, 0);
+            // graph1.AddEdge(0, -1);
+            // graph1.AddEdge(0, graph1.numVertices);
+
+            Console.WriteLine("\nHere's a breadth first traversal of a directed graph with an adjancency list and 4 vertices starting from vertex 2:");
             graph1.PrintVerticesBreathFirst(2);
+
+            Console.WriteLine("\n\nSame graph, same method, starting vertex is -1:");
+            graph1.PrintVerticesBreathFirst(-1);
+
+            Console.WriteLine($"\n\nSame graph, same method, starting vertex is {graph1.numVertices}:");
+            graph1.PrintVerticesBreathFirst(graph1.numVertices);
+
+            // Produce a Null Reference Exception that doesn't reach the method.
+            // graph1 = null;
+            // graph1.PrintVerticesBreathFirst(0);
 
             Console.WriteLine();
         }
