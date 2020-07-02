63284672254c5e6a29caf4ae55ded6af510e2120
SuperJuice <wisconsinsara@gmail.com>
2020-07-01 20:15:19 -0700

    Started GraphDirectedAdjacencyMatrix

---
+++ SelfStudyGraphs/GraphDirectedAdjacencyMatrix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace SelfStudyGraphs
+{
+    class GraphDirectedAdjacencyMatrix
+    {
+        private bool[,] adj;
+        private int numVertices;
+
+        public GraphDirectedAdjacencyMatrix(int numVertices)
+        {
+            // Check for at least 1 vertex.
+            if (numVertices < 1)
+            {
+                throw new ArgumentOutOfRangeException("Graph must have at least 1 vertex.");
+            }
+
+            this.numVertices = numVertices;
+        }
+    }
+}
